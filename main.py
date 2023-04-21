4c8f493bb97362953d66b3fa96123539038e3631
GCorreia3 <2218@rgsg.co.uk>
2023-04-21 22:44:49 +0100

    Working on new laser class

--- main.py
+++ main.py
@@ -9,8 +9,43 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.init()
 
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
 def dist_to(pos1, pos2):
     return ((pos2[0] - pos1[0])**2 + (pos2[1] - pos1[1])**2)**0.5
+
+
+
+def wavelength_to_colour(wavelength, amplitude):
+    if wavelength >= 380 and wavelength <= 460:
+        R = (460 - wavelength) / (460 - 350)
+        G = 0
+        B = 1
+    elif wavelength >= 460 and wavelength <= 510:
+        R = 0
+        G = (wavelength - 460) / (510 - 460)
+        B = 1
+    elif wavelength >= 510 and wavelength <= 550:
+        R = 0
+        G = 1
+        B = (550 - wavelength) / (550 - 510)
+    elif wavelength >= 550 and wavelength <= 600:
+        R = (wavelength - 550) / (600 - 550)
+        G = 1
+        B = 0
+    elif wavelength >= 600 and wavelength <= 700:
+        R = 1
+        G = (700 - wavelength) / (700 - 600)
+        B = 0
+    else:
+        R = 1
+        G = 1
+        B = 1
+
+    return (int(R * 255 * amplitude), int(G * 255 * amplitude), int(B * 255 * amplitude))
 
 
 
@@ -90,6 +125,35 @@ class Mirror(RotatedRectangle):
 
 
 
+class Laser():
+    def __init__(self, pos, width, length, amplitude, wavelength, direction) -> None:
+
+        self.x, self.y = pos
+        self.width = width
+        self.length = length
+
+        self.amplitude = amplitude
+
+        self.wavelength = wavelength
+        self.speed = 3_000_000_000
+        self.frequency = self.speed / (wavelength / 1_000_000_000)
+
+        self.colour = wavelength_to_colour(self.wavelength, self.amplitude)
+
+        self.direction = direction
+
+    def draw(self):
+        if self.direction == UP:
+            pygame.draw.rect(WIN, self.colour, (self.x - self.width/2, self.y - self.length, self.width, self.length))
+        elif self.direction == RIGHT:
+            pygame.draw.rect(WIN, self.colour, (self.x, self.y - self.width/2, self.length, self.width))
+        elif self.direction == DOWN:
+            pygame.draw.rect(WIN, self.colour, (self.x - self.width/2, self.y, self.width, self.length))
+        elif self.direction == LEFT:
+            pygame.draw.rect(WIN, self.colour, (self.x - self.length, self.y - self.width/2, self.length, self.width))
+
+
+
 class Interferometer():
     def __init__(self, wavelength) -> None:
 
@@ -97,6 +161,8 @@ class Interferometer():
 
         self.top_mirror = Mirror((WIDTH/2, HEIGHT/8), 50, 10, 0, (100, 200, 255), 1, 5, HEIGHT/2-25)
         self.right_mirror = Mirror((7*WIDTH/8, HEIGHT/2), 50, 10, 90, (100, 200, 255), 0, WIDTH/2+25, WIDTH-5)
+
+        self.laser_emitted = Laser((WIDTH/4-25 + 50, HEIGHT/2-25/2 + 12.5), 10, 400, 0.5, 700, RIGHT)
 
         self.path_difference = abs(self.top_mirror.path_distance - self.right_mirror.path_distance)
 
@@ -132,14 +198,16 @@ class Interferometer():
         pygame.draw.rect(WIN, (255, 255, 255), (WIDTH/2-25/2, 3*HEIGHT/4-25/2, 25, 25))
 
         # Draw First Laser
-        pygame.draw.rect(WIN, (255, 0, 0), (WIDTH/4+25, HEIGHT/2-5, WIDTH/4-25, 10))
+        #pygame.draw.rect(WIN, (255, 0, 0), (WIDTH/4+25, HEIGHT/2-5, WIDTH/4-25, 10))
 
         # Draw Both Split Lasers
-        pygame.draw.rect(WIN, (255/2, 0, 0), (WIDTH/2, HEIGHT/2-5, self.right_mirror.pos[0]-WIDTH/2, 10))
-        pygame.draw.rect(WIN, (255/2, 0, 0), (WIDTH/2-5, self.top_mirror.pos[1], 10, HEIGHT/2-self.top_mirror.pos[1]))
+        #pygame.draw.rect(WIN, (255/2, 0, 0), (WIDTH/2, HEIGHT/2-5, self.right_mirror.pos[0]-WIDTH/2, 10))
+        #pygame.draw.rect(WIN, (255/2, 0, 0), (WIDTH/2-5, self.top_mirror.pos[1], 10, HEIGHT/2-self.top_mirror.pos[1]))
 
         # Draw Resultant Laser
         pygame.draw.rect(WIN, (255 * self.phase_difference, 0, 0), (WIDTH/2-5, HEIGHT/2, 10, HEIGHT/4-25/2))
+
+        self.laser_emitted.draw()
 
         # Draw Two Mirrors
         self.top_mirror.draw()
